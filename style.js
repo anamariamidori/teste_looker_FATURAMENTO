163999bb02bb6c01f5f8e17a8ba27fd12055ea8d
Midori <87765397+anamariamidori@users.noreply.github.com>
2025-03-28 14:58:27 +0000

    criação js

---
+++ style.js
@@ -0,0 +1,4 @@
+document.querySelectorAll("td[data-title='Novo'], td[data-title='Usado'], td[data-title='Venda Direta']").forEach(td => {
+    td.style.color = "blue";
+    td.style.fontWeight = "bold";
+});
